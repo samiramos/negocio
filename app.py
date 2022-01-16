f46bec0a10598fe2be706a862932ca3a7e6c5072
Samira <62807652+samiramos@users.noreply.github.com>
2022-01-16 21:08:43 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+local = input("colocar local no feminino: ")
+animal = input("colocar animal no masculino: ")
+objeto = input("objeto no feminino ")
+fala_exdruxula = input("Fala exdrúxula")
+
+print("Certo dia eu estava na " + local + " fazendo compras para o meu " + animal)
+print("quando derrepente eu vi um " + objeto + " Sendo arremessada no ar!")
+print("Daí eu falei: " + fala_exdruxula)
